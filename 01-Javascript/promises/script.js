953f5f2c1d3dc77aaffeace4df97026dbbc71b86
William Barbosa <wsbprofissional@gmail.com>
2026-06-24 23:10:45 -0300

    aulas promise js

---
+++ 01-Javascript/promises/script.js
@@ -0,0 +1,43 @@
+// PRIMEIRO EXEMPLO DE PROMISE
+/*
+
+PROMISE ETAPAS
+
+- pending
+- fulfilled = executa -> then()
+- rejected  = executa -> catch()
+- finally -> executar independente
+
+ */
+
+// Função que retorna uma Promise
+function darCerto() {
+   return new Promise((resolve, reject) => {
+      // Espera 3 segundos
+      setTimeout(() => {
+         resolve("Success!"); // Está testando o "then"
+         //  reject("Error!"); // Está testando o "catch"
+      }, 3000);
+   });
+}
+
+let button = document.querySelector("button").addEventListener("click", () => {
+   // Executando a Promise
+   darCerto()
+      .then((success) => {
+         // Se der certo (fulfilled)
+         console.log(success);
+         let teste = document.querySelector("#texto");
+         teste.innerText = "deu certo";
+      })
+      .catch((error) => {
+         // Se der erro (rejected)
+         console.log(error);
+         let teste = document.querySelector("#texto");
+         teste.innerText = "Deu errado";
+      })
+      .finally(() => {
+         // Executa sempre, no final
+         console.log("Finalizando...");
+      });
+});
